16536fd0322c80914becbd174544886545ac7ac6
Paul Visscher <p.e.visscher@zefiros.io>
2022-01-11 19:12:15 +0100

    chore: allow release from next

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    branches: [{ name: 'main' }, { name: 'feat/node-standards', channel: 'next', prerelease: 'beta' }],
+    branches: [{ name: 'main' }, { name: 'next', channel: 'next', prerelease: 'beta' }],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
