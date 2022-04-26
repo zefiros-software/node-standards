8a64374f38481cd10d70bf728f950a8d5cd3e1c4
Paul Visscher <p.e.visscher@zefiros.io>
2022-04-26 12:20:10 +0200

    fix: skip js linting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': (filenames) =>
-        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/strict-boolean-expressions
-        filenames.length > 10 ? 'npm run fix' : `npm run fix -- ${filenames.filter((f) => !f.includes('.config.')).join(' ')}`,
-    '*.{json,yml,yaml}': 'prettier --write',
+    '*.{ts,tsx}': (filenames) =>
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        filenames.length > 10 ? 'npm run fix' : `npm run fix -- ${filenames.join(' ')}`,
+    '*.{json,js,jsx,yml,yaml}': 'prettier --write',
 }
