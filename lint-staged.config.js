c9b60697e7a36d7a8155a20f49c86e5ef64924b9
MickVanDuijn <m.vanduijn@zefiros.io>
2022-04-29 09:51:30 +0200

    feat(ci): add project linting job (#14)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,5 +2,6 @@ module.exports = {
     '*.{ts,tsx}': (filenames) =>
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
         filenames.length > 10 ? 'npm run fix' : `npm run fix -- ${filenames.join(' ')}`,
-    '*.{json,js,jsx,yml,yaml}': 'prettier --write',
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/strict-boolean-expressions
+    '*.{json,yml,yaml}': (filenames) => `prettier --write ${filenames.filter((f) => !f.endsWith('package-lock.json').join(' '))}`,
 }
